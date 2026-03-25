c29a0711fb3dababae6176ebf0540e729ca9a342
Kingrosez <157677497+Kingrosez@users.noreply.github.com>
2026-03-25 20:57:56 +0530

    Delete user-service directory

--- user-service/src/main/java/com/mfp/user_service/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.mfp.user_service.enums;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- user-service/src/main/java/com/mfp/user_service/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mfp.user_service.exception;
-
-public class UserNotFoundException extends BaseException{
-    public UserNotFoundException(Long id) {
-        super("User with id " + id + " not found","USER_NOT_FOUND");
-    }
-}
